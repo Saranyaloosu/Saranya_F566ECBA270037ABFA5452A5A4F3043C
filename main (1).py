1306fcc4e169a67de63dda8561afb908202b433b
Saranyaloosu <144884875+Saranyaloosu@users.noreply.github.com>
2023-09-13 12:38:45 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,8 @@
+class Base_1:     #Base Class 1
+    pass
+
+class Base_2:      #Base Class 2
+    pass
+
+class Derived(Base_1,Base_2):      #Class that derives from both the above base classes
+    pass
